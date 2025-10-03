cfb49a18fcb60e32d5a722a71885f83d44e19447
Rafa <157777055+bets18@users.noreply.github.com>
2025-10-03 23:36:16 +0000

    lab6

---
+++ lab6/ex1.s
@@ -0,0 +1,37 @@
+/*
+int nums[] = {10, -21, -30, 45};
+int main() {
+  int i, *p;
+  int sum = 0;
+  for (i = 0, p = nums; i != 4; i++, p++)
+    sum += *p;
+  printf("%d\n", sum);
+  return 0;
+}
+*/
+
+.data
+nums:  .int  10, -21, -30, 45
+Sf:  .string "%d\n"    # string de formato para printf
+
+.text
+.globl  main
+main:
+
+  pushq   %rbp
+  movq    %rsp, %rbp
+  subq    $16, %rsp
+  movq    %rbx, -8(%rbp)
+  movq    %r12, -16(%rbp)
+
+  movl  $0, %ebx  /* ebx = 0; */
+  movq  $nums, %r12  /* r12 = &nums */
+  movl $0, %eax
+
+  L1:
+  cmpl  $4, %ebx  /* if (ebx == 4) ? */
+  je  L2          /* goto L2 */
+
+  movq    $Sf, %rdi    /* primeiro parametro (ponteiro)*/
+  movl    %eax, %esi   /* segundo parametro  (inteiro) */
+  call  printf       /* chama a funcao da biblioteca */
