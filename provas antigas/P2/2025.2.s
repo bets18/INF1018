86b5ffc1c4e2d6cce30816ea38d95ed30cb8159d
Rafa <157777055+bets18@users.noreply.github.com>
2025-11-24 19:24:02 +0000

    quase acabando 2025.2

---
+++ provas antigas/P2/2025.2.s
@@ -0,0 +1,49 @@
+# POSSIVEIS PROVAS QUE O THIAGO FEZ
+
+# P1
+.text 
+.globl foo 
+
+# dicionario 
+# float a   %xmm0
+# int b     %edi  -> %xmm1
+
+foo:
+    pushq   %rbp 
+    movq    %rsp, %rbp 
+    subq    $16, %rsp
+
+    cvtss2sd    %xmm0, %xmm0
+
+    movss   %xmm0, -8(%rbp)
+    
+    cvtsi2ss  %edi, %xmm1
+    movss   %xmm1, -16(%rbp)
+
+    call    sin 
+
+    addss   -16(%rbp), %xmm0
+    leave 
+    ret
+
+.text 
+.globl pack 
+
+# dicionario
+# struct X *px  %edi 
+
+pack: 
+    pushq   %rbp 
+    movq    %rsp, %rbp
+    subq    $16, %rsp
+
+    pxor    %xmm0, %xmm0
+    movsd   %xmm0, -8(%rbp) # double acc
+
+while:
+    cmp     %edi, $0
+    je      fim
+
+    mov
+
+
